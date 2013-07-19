63f4d42ffc71d02947f60322594e285f6f170556
Arie <arie@wego.com>
2013-07-20 01:50:51 +0700

    add common

---
+++ tripify/library/common/tripify.h
@@ -0,0 +1,17 @@
+//
+//  tripify.h
+//  tripify
+//
+//  Created by Arie on 7/20/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tripify : NSObject
+extern NSString * const api_key;
+extern NSString * const hotel_url;
+extern NSString * const flight_url;
+extern NSString * const deal_filter;
+extern NSString * const deal_url;
+@end
